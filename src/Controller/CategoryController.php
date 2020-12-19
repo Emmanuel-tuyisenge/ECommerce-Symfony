dd83eab5e1fc8705955081e2221cfaa27f6cd112
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2020-12-19 12:03:55 +0100

    introduction od security in this project

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,11 +6,16 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategoryController extends AbstractController
 {
@@ -61,14 +66,39 @@ class CategoryController extends AbstractController
 
     /**
      * @Route("/admin/category/{id}/edit", name="category_edit")
+     * //@IsGranted("ROLE_ADMIN", message="Vous n'avez pas le droit d'accéder à cette ressource")
      */
     public function edit(
         $id,
         CategoryRepository $categoryRepository,
         Request $request,
-        EntityManagerInterface $em
+        EntityManagerInterface $em,
+        Security $security
     ) {
+        //$category = $categoryRepository->find($id);
+
+        //$this->denyAccessUnlessGranted("CAN_EDIT", null, "Vous n'êtes pas le propriétaire de cette catégorie");
+
+        //$this->denyAccessUnlessGranted("ROLE_ADMIN", null, "Vous n'avez pas le droit d'accéder à cette ressource");
+
+        // $user = $security->getUser();
+
+        // if ($user === null) {
+        //     return $this->redirectToRoute('security_login');
+        // }
+
+        // if (!in_array("ROLE_ADMIN", $user->getRoles())) {
+        //     throw new AccessDeniedHttpException("Vous n'avez pas le droit d'accéder à cette ressource");
+        // }
+        // if ($this->isGranted("ROLE_ADMIN") === false) {
+        //         throw new AccessDeniedHttpException("Vous n'avez pas le droit d'accéder à cette ressource");
+        //     }
+
         $category = $categoryRepository->find($id);
+
+        if (!$category) {
+            throw new NotFoundHttpException("Cette catégorie n'existe pas !");
+        }
 
         $form = $this->createForm(CategoryType::class, $category);
 
---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class LoginFormAuthenticator extends AbstractGuardAuthenticator
+{
+    protected $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function supports(Request $request)
+    {
+        return $request->attributes->get('_route') === 'security_login' && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return $request->request->get('login');
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        try {
+            return $userProvider->loadUserByUsername($credentials['email']);
+        } catch (UsernameNotFoundException $e) {
+            throw new AuthenticationException("Cette adresse email n'est pas connue");
+        }
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        $isValid = $this->encoder->isPasswordValid($user, $credentials['password']);
+
+        if (!$isValid) {
+            throw new AuthenticationException("Le password est incorrecté");
+        }
+
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $request->attributes->set(Security::AUTHENTICATION_ERROR, $exception);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    {
+        return new RedirectResponse('/');
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // todo
+    }
+
+    public function supportsRememberMe()
+    {
+        // todo
+    }
+}
